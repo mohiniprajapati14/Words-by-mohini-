4ca273de7f06583db1e5ba24a8a26aa041393224
mohiniprajapati14 <jaiwalpriti448@gmail.com>
2026-07-02 23:35:43 +0530

    Delete Hero.tsx

--- Hero.tsx
+++
@@ -1,7 +0,0 @@
-export default function Hero(){
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <h1 className="text-5xl gold-text">Luxury Copywriting</h1>
-    </div>
-  );
-}
