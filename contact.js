486c487364ef8abf05c38c90490aa839840dfee7
mohiniprajapati14 <jaiwalpriti448@gmail.com>
2026-07-02 17:33:42 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,17 @@
+export default function Contact(){
+  return (
+    <div>
+      <div className="nav">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/services">Services</a>
+        <a href="/contact">Contact</a>
+      </div>
+
+      <div className="hero">
+        <h1>Contact</h1>
+        <p>Email: contact@wordsbymohini.com</p>
+      </div>
+    </div>
+  )
+}
