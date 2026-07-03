0fe585cde18f9f8c45dda36497e4d775c9a10865
mohiniprajapati14 <jaiwalpriti448@gmail.com>
2026-07-03 13:07:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-function scrollToSection(id){
-  document.getElementById(id).scrollIntoView({behavior:'smooth'});
-}
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if(entry.isIntersecting){
-      entry.target.classList.add('active');
-    }
-  });
-});
-
-document.querySelectorAll('.reveal').forEach(el => observer.observe(el));
+console.log("Portfolio Loaded Successfully");
